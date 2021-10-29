9f5d7b6755904fba13c07dd0b7078c2d48e5c879
SunnyDhanwani <dhanwanisunny678@gmail.com>
2021-10-29 10:36:50 +0530

    Added avatar

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import myImg from "../../Assets/avatar.svg";
+import myImg from "../../Assets/avatar.png";
 import Tilt from "react-parallax-tilt";
 import {
   AiFillGithub,
@@ -26,7 +26,7 @@ function Home2() {
           </Col>
           <Col md={4} className="myAvtar">
             <Tilt>
-              <img src="https://drive.google.com/file/d/1uYkPamy5BuYsb70fJAEHzozXrNEat45j/view?usp=sharing" className="img-fluid" alt="avatar" />
+              <img src={myImg} className="img-fluid" alt="avatar" />
             </Tilt>
           </Col>
         </Row>
