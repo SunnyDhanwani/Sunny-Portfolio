b7d66c601cb7d5c1843b662ab4efca4f81faebc6
Sunny Dhanwani <dhanwanisunny678@gmail.com>
2023-08-30 09:05:39 +0530

    Updated projects sectin

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,10 +3,11 @@ import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 
-import bluemercury from "../../Assets/Projects/bluemercury.png";
-import yatra from "../../Assets/Projects/yatra.png";
 import precize from "../../Assets/Projects/precizePreview.jpeg";
 import pazago from "../../Assets/Projects/pazagoPreview.png";
+import propaze from "../../Assets/Projects/propazePreview.png";
+import precizeWebapp from "../../Assets/Projects/precizeWebappPreview.png";
+import pazagoWebapp from "../../Assets/Projects/pazagoWebappPreview.png";
 
 function Projects() {
   return (
@@ -34,9 +35,44 @@ function Projects() {
               imgPath={pazago}
               isBlog={false}
               title="Pazago"
-              description="Pazago makes trade between 90 different countries painless, ensuring effortless international payments, streamlined compliance, efficient financing, and an optimized supply chain."
+              description="Pazago makes trade between 90 different countries painless, ensuring effortless international payments, streamlined compliance, efficient financing, and an optimized supply chain"
               gitLink="https://github.com/neerajmasai/yatra-clone"
               liveLink="https://www.pazago.com/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={propaze}
+              isBlog={false}
+              title="Propaze"
+              description="Reliable cross-border logistics with end-to-end solutions for exporters and importers"
+              gitLink="https://github.com/neerajmasai/yatra-clone"
+              liveLink="https://www.propaze.com/"
+            />
+          </Col>
+        </Row>
+
+        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={precizeWebapp}
+              isBlog={false}
+              title="Precize Portal"
+              description="Precize portal is a world-class unlisted share market gateway where investors of all levels can buy and sell unlisted shares online securely."
+              gitLink=""
+              liveLink="https://portal.precize.in/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={pazagoWebapp}
+              isBlog={false}
+              title="Pazago Webapp"
+              description="Pazago webapp makes trade between 90 different countries painless, ensuring effortless international payments, streamlined compliance, efficient financing, and an optimized supply chain"
+              gitLink="https://github.com/neerajmasai/yatra-clone"
+              liveLink="https://app.pazago.com/"
             />
           </Col>
         </Row>
